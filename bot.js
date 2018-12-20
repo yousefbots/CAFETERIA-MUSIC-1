f6a47549bafcc5989915e719a6b2e4f4046a728e
yousefbots <45495875+yousefbots@users.noreply.github.com>
2018-12-20 18:57:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`#play | CAFETERIA SERVER`,"http://twitch.tv/CAFETERIA SERVER")
+client.user.setGame(`#play | Dream Killer`,"http://twitch.tv/Dream Killer")
 client.user.setStatus("dnd")
 });
 
